cfdf8475f2d7d88258c22a0bfe97c80a3def58c1
krzyswys <krzyswys3@gmail.com>
2023-03-21 22:34:48 +0100

    MES

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+import org.example.GUI.App;
+import javafx.application.Application;
+public class Main {
+
+    public static void main(String[] args) {
+        Application.launch(App.class, args);
+
+    }
+}
